d7d9c3be1f994279614122f4fd4a797bc9dde177
Egor <121961504+Spaceonmymind@users.noreply.github.com>
2023-12-04 01:03:23 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+
+from logging import config
+config.fileConfig('dev.logger.conf')
+from PaymentsJournal import PaymentsJournal
+
+driver = webdriver.Chrome()
+
+parser = PaymentsJournal(driver)
+docs = parser.content()
+
+
+print(*docs, sep='\n\r\n')
