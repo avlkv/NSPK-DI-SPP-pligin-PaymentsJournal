568f2a5613f07d9001a0a0520922bbb96366132d
Artyom <volkovartik@gmail.com>
2024-03-22 17:37:37 +0300

    main funcs done, need config and cleaning

--- PaymentsJournal.py
+++ PaymentsJournal.py
@@ -4,15 +4,15 @@
 1/2 документ плагина
 """
 import logging
-import os
 import time
 
 import dateparser
+from datetime import datetime
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.chrome.webdriver import WebDriver
 from src.spp.types import SPP_document
-
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 class PaymentsJournal:
     """
     Класс парсера плагина SPP
@@ -30,7 +30,8 @@ class PaymentsJournal:
     HOST = "https://www.paymentsjournal.com/news/"
     _content_document: list[SPP_document]
 
-    def __init__(self, driver, *args, **kwargs):
+    def __init__(self, webdriver: WebDriver, last_document: SPP_document = None, max_count_documents: int = 100, *args,
+                 **kwargs):
         """
         Конструктор класса парсера
 
@@ -39,18 +40,14 @@ class PaymentsJournal:
         """
         # Обнуление списка
         self._content_document = []
-        self.driver = driver
+
+        self.driver = webdriver
+        self.wait = WebDriverWait(self.driver, timeout=20)
+        self.max_count_documents = max_count_documents
+        self.last_document = last_document
 
         # Логер должен подключаться так. Вся настройка лежит на платформе
         self.logger = logging.getLogger(self.__class__.__name__)
-
-        # УДалить DRAFT
-        logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
-        consoleHandler = logging.StreamHandler()
-        consoleHandler.setFormatter(logFormatter)
-        self.logger.addHandler(consoleHandler)
-        #
-
         self.logger.debug(f"Parser class init completed")
         self.logger.info(f"Set source: {self.SOURCE_NAME}")
         ...
@@ -62,8 +59,12 @@ class PaymentsJournal:
         :rtype:
         """
         self.logger.debug("Parse process start")
-        self._parse()
-        self.logger.debug("Parse process finished")
+        try:
+            self._parse()
+        except Exception as e:
+            self.logger.debug(f'Parsing stopped with error: {e}')
+        else:
+            self.logger.debug("Parse process finished")
         return self._content_document
 
     def _parse(self):
@@ -79,11 +80,10 @@ class PaymentsJournal:
         # Тут должен находится блок кода, отвечающий за парсинг конкретного источника
         # -
 
-        self.driver.get(
-            "https://www.paymentsjournal.com/news/")  # Открыть первую страницу с материалами EMVCo в браузере
-        time.sleep(10)
+        self.driver.get(self.HOST)  # Открыть первую страницу с материалами EMVCo в браузере
+        time.sleep(3)
 
-        # Cooskies
+        # Cookies
         try:
             cookies_btn = self.driver.find_element(By.ID, 'normal-slidedown').find_element(By.XPATH,
                                                                                            '//*[@id="onesignal-slidedown-allow-button"]')
@@ -133,90 +133,45 @@ class PaymentsJournal:
         while flag:
             self.driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
             self.logger.debug('Загрузка списка элементов...')
-            doc_table = self.driver.find_element(By.CLASS_NAME, 'jeg_posts ').find_elements(By.TAG_NAME,
-                                                                                            'article')
-            self.logger.debug(f'Обработка списка элементов ({len(doc_table)})...')
-
-            # Цикл по всем строкам таблицы элементов на текущей странице
-            for element in doc_table:
-
-                element_locked = False
-
-                try:
-                    title = element.find_element(By.CLASS_NAME, 'jeg_post_title').text
-                    # title = element.find_element(By.XPATH, '//*[@id="feed-item-title-1"]/a').text
-
-                except:
-                    self.logger.exception('Не удалось извлечь title')
-                    title = ' '
-
-                try:
-                    other_data = element.find_element(By.CLASS_NAME, "by").text
-                except:
-                    self.logger.exception('Не удалось извлечь other_data')
-                    other_data = ''
-                try:
-                    date = dateparser.parse(element.find_element(By.CLASS_NAME, 'jeg_meta_date').text)
-                except:
-                    self.logger.exception('Не удалось извлечь date')
-                    date = ' '
-
-                # try:
-                #    date = dateparser.parse(date_text)
-                # except:
-                #    self.logger.exception('Не удалось извлечь date')
-                #    date = None
-
-                try:
-                    # abstract = element.find_element(By.CLASS_NAME, '').text
-                    abstract = ' '
-                except:
-                    # self.logger.exception('Не удалось извлечь abstract')
-                    abstract = ' '
-
-                book = ' '
-
-                try:
-                    web_link = element.find_element(By.TAG_NAME, 'a').get_attribute('href')
-                except:
-                    self.logger.exception('Не удалось извлечь web_link')
-                    web_link = None
-
-                self._content_document.append(SPP_document(
-                    doc_id=None,
-                    title=title,
-                    abstract=abstract,
-                    text=None,
-                    web_link=web_link,
-                    local_link=None,
-                    other_data=other_data,
-                    pub_date=date,
-                    load_date=None,
-                ))
 
             counter = 0
 
             try:
                 # Get scroll height
-                last_height = self.driver.execute_script("return document.body.scrollHeight")
+                doc_table = self.driver.find_elements(By.TAG_NAME, 'article')
+                last_doc_table_len = len(doc_table)
 
                 while True:
                     # Scroll down to bottom
                     self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
                     counter += 1
-                    print(counter)
+                    self.logger.info(f"counter = {counter}")
 
                     # Wait to load page
-                    time.sleep(0.5)
+                    time.sleep(1)
+
+                    try:
+                        self.driver.execute_script('arguments[0].click()', self.driver.find_element(By.XPATH, '//*[contains(@class,\'dialog-close-button\')]'))
+                    except:
+                        self.logger.exception('Не найдена реклама')
+
+                    # Wait to load page
+                    time.sleep(1)
+
+                    self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+                    # Wait to load page
+                    time.sleep(1)
 
                     # Calculate new scroll height and compare with last scroll height
-                    new_height = self.driver.execute_script("return document.body.scrollHeight")
-                    if new_height == last_height:
+                    doc_table = self.driver.find_elements(By.TAG_NAME, 'article')
+                    new_doc_table_len = len(doc_table)
+                    if last_doc_table_len == new_doc_table_len:
                         break
-                    if counter > 100:
+                    if counter > 1:
                         flag = False
                         break
-                    last_height = new_height
+
 
                     try:
                         reg_btn = self.driver.find_element(By.CLASS_NAME, 'dialog-widget-content').find_element(
@@ -235,33 +190,69 @@ class PaymentsJournal:
                 self.logger.exception('Не удалось найти scroll')
                 break
 
-            # try:
-            #    pagination_arrow = self.driver.find_element(By.XPATH, '//a[contains(@data-direction,\'next\')]')
-            #    self.driver.execute_script('arguments[0].click()', pagination_arrow)
-            #    time.sleep(3)
-            #    pg_num = self.driver.find_element(By.ID, 'current_page').text
-            #    self.logger.info(f'Выполнен переход на след. страницу: {pg_num}')
 
-            #                 if int(pg_num) > 5:
-            #                     self.logger.info('Выполнен переход на 6-ую страницу. Принудительное завершение парсинга.')
-            #                     break
 
-            # try:
-            #    while self.driver.find_element(By.TAG_NAME,'div'):
-            #        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            #        Divs = self.driver.find_element(By.TAG_NAME,'div').text
-            #        if 'End of Results' in Divs:
-            #            print
-            #            'end'
-            #            break
-            #        else:
-            #            continue
-            # except:
-            #    self.logger.exception('Не удалось найти переход на след. страницу. Прерывание цикла обработки')
-            #    break
+            self.logger.debug(f'Обработка списка элементов ({len(doc_table)})...')
 
-        # Логирование найденного документа
-        # self.logger.info(self._find_document_text_for_logger(_content_document))
+            # Цикл по всем строкам таблицы элементов на текущей странице
+            for element in doc_table:
+
+                element_locked = False
+
+                try:
+                    title = element.find_element(By.CLASS_NAME, 'jeg_post_title').text
+                    # title = element.find_element(By.XPATH, '//*[@id="feed-item-title-1"]/a').text
+
+                except:
+                    self.logger.exception('Не удалось извлечь title')
+                    continue
+
+                try:
+                    web_link = element.find_element(By.CLASS_NAME, 'jeg_post_title').find_element(By.TAG_NAME, 'a').get_attribute('href')
+                except:
+                    self.logger.exception('Не удалось извлечь web_link')
+                    web_link = None
+
+                try:
+                    other_data = {'author': element.find_element(By.CLASS_NAME, "jeg_meta_author").find_element(By.TAG_NAME, 'a').text}
+                except:
+                    self.logger.exception('Не удалось извлечь other_data')
+                    other_data = {}
+
+                self.driver.execute_script("window.open('');")
+                self.driver.switch_to.window(self.driver.window_handles[1])
+
+                self.driver.get(web_link)
+                self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.jeg_post_title')))
+
+                try:
+                    pub_date = dateparser.parse(self.driver.find_element(By.CLASS_NAME, 'jeg_meta_date').text)
+                except:
+                    self.logger.exception('Не удалось извлечь date')
+                    continue
+
+                try:
+                    text_content = dateparser.parse(self.driver.find_element(By.CLASS_NAME, 'content-inner ').text)
+                except:
+                    self.logger.exception('Не удалось извлечь text')
+                    continue
+
+                abstract = ''
+
+                doc = SPP_document(None,
+                                   title,
+                                   abstract,
+                                   text_content,
+                                   web_link,
+                                   None,
+                                   other_data,
+                                   pub_date,
+                                   datetime.now())
+
+                self.find_document(doc)
+
+                self.driver.close()
+                self.driver.switch_to.window(self.driver.window_handles[0])
 
         # ---
         # ========================================
@@ -277,52 +268,15 @@ class PaymentsJournal:
         """
         return f"Find document | name: {doc.title} | link to web: {doc.web_link} | publication date: {doc.pub_date}"
 
-    @staticmethod
-    def some_necessary_method():
+    def find_document(self, _doc: SPP_document):
         """
-        Если для парсинга нужен какой-то метод, то его нужно писать в классе.
-
-        Например: конвертация дат и времени, конвертация версий документов и т. д.
-        :return:
-        :rtype:
+        Метод для обработки найденного документа источника
         """
-        ...
+        if self.last_document and self.last_document.hash == _doc.hash:
+            raise Exception(f"Find already existing document ({self.last_document})")
 
-    @staticmethod
-    def nasty_download(driver, path: str, url: str) -> str:
-        """
-        Метод для "противных" источников. Для разных источника он может отличаться.
-        Но основной его задачей является:
-            доведение driver селениума до файла непосредственно.
+        if self.max_count_documents and len(self._content_document) >= self.max_count_documents:
+            raise Exception(f"Max count articles reached ({self.max_count_documents})")
 
-            Например: пройти куки, ввод форм и т. п.
-
-        Метод скачивает документ по пути, указанному в driver, и возвращает имя файла, который был сохранен
-        :param driver: WebInstallDriver, должен быть с настроенным местом скачивания
-        :_type driver: WebInstallDriver
-        :param url:
-        :_type url:
-        :return:
-        :rtype:
-        """
-
-        with driver:
-            driver.set_page_load_timeout(40)
-            driver.get(url=url)
-            time.sleep(1)
-
-            # ========================================
-            # Тут должен находится блок кода, отвечающий за конкретный источник
-            # -
-            # ---
-            # ========================================
-
-            # Ожидание полной загрузки файла
-            while not os.path.exists(path + '/' + url.split('/')[-1]):
-                time.sleep(1)
-
-            if os.path.isfile(path + '/' + url.split('/')[-1]):
-                # filename
-                return url.split('/')[-1]
-            else:
-                return ""
+        self._content_document.append(_doc)
+        self.logger.info(self._find_document_text_for_logger(_doc))
